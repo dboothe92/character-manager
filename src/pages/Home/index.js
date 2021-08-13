2683ccd96f8fa60c6b1b1b2b4786024be45291c9
Dan B <dbraunby@hotmail.com>
2021-08-13 11:22:26 -0600

    homepage layout and base design

---
+++ src/pages/Home/index.js
@@ -0,0 +1,17 @@
+const Home = () => {
+    return (
+        <section className="home">
+            <button type="button">Login</button>
+            <button type="button">Sign Up</button>
+
+            <p className="home-description">
+                Welcome to the last character manager you'll ever need!
+            </p>
+            <p className="home-edition">
+                Currently servicing Dungeons and Dragons 5E
+            </p>
+        </section>
+    )
+}
+
+export default Home;
